ccc752e9bb2d9bd785b0b02f5acfe8240b18b92b
KarineVerneque <karinemvs01@hotmail.com>
2020-01-30 16:25:15 -0300

    creating products table

--- models/index.js
+++ models/index.js
@@ -1,11 +1,14 @@
 'use strict';
-
+import configJson from '../api/server/src/config/config'
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const config = configJson[env]
+// const config = require(__dirname + '/home/laboratoria/lab/SAP003-burger-queen-api/api/server/src/config/config.js')[env];
+
 const db = {};
 
 let sequelize;
